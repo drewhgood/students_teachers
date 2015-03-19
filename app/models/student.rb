13e173485a0aaec184538136b1c0f410b5ac9e75
Andrew Good <drewhgood@gmail.com>
2015-03-19 12:07:57 -0700

    Method to auto assigne students equally implimented.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,12 @@
 class Student < ActiveRecord::Base
   belongs_to :teacher
+
+  def assign
+   teacher = Teacher.least_students
+   student = self
+   student.teacher = teacher
+   student.save!
+  end
 end
+
+
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -3,16 +3,16 @@ class Teacher < ActiveRecord::Base
   validates :email, uniqueness: true
 
 
-# # def self.least_students
-# #   teachers = Teacher.all
-# #   asignee = Teacher.first
+  def self.least_students
+    teachers = Teacher.all
+    asignee = Teacher.first
 
-# #   teachers.each do |teacher|
-# #     asignee = teacher if teacher.students.count <= asignee.students.count 
-# #   end
+    teachers.each do |teacher|
+      asignee = teacher if teacher.students.count <= asignee.students.count 
+    end
 
-# #   asignee
-# # end
+    asignee
+  end
 
 
 end
